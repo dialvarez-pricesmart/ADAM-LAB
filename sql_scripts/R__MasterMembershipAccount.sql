a3e9f41cc2c6a7da2f5311f04fa1cc3b802e4b1b
majimenez14 <majimenez@pricesmart.com>
2025-07-14 13:25:20 -0600

    feat (Membership): Added Account Type

--- sql_scripts/R__MasterMembershipAccount.sql
+++ sql_scripts/R__MasterMembershipAccount.sql
@@ -1,11 +1,13 @@
 CREATE OR REPLACE TABLE "MASTER_membership_account" (
 
-"Cost_Center"
+"Cost_Center",
+"Account_Type"
 
 
 ) AS
 
 SELECT 
-TRIM("ACMCU")
+TRIM("ACMCU"),
+TRIM("AC$ATY") AS “Account_Type”
 FROM psmt_lakehouse_dev.dataops_training1."RAW_membership_accounts_f55634"
 ;
